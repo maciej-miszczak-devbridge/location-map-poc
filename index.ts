9bd21d266e1d818ef7fbc5b7bfb22e00d27d0d03
maciej-miszczak-devbridge <111114156+maciej-miszczak-devbridge@users.noreply.github.com>
2022-08-30 11:41:08 +0200

    Pie slices turned off

--- index.ts
+++ index.ts
@@ -4,8 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { MarkerClusterer } from "@googlemaps/markerclusterer";
-
 let map: google.maps.Map;
 
 function initMap(): void {
@@ -21,31 +19,31 @@ function initMap(): void {
       "0": {
         rasterIcon: {
           url: "covered_location.png",
-          scaledSize: new google.maps.Size(5, 5)
+          scaledSize: new google.maps.Size(2, 2)
         }
       },
       "1": {
         rasterIcon: {
           url: "covered_location.png",
-          scaledSize: new google.maps.Size(8, 8)
+          scaledSize: new google.maps.Size(3, 3)
         }
       },
       "2": {
         rasterIcon: {
           url: "covered_location.png",
-          scaledSize: new google.maps.Size(12, 12)
+          scaledSize: new google.maps.Size(6, 6)
         }
       },
       "3": {
         rasterIcon: {
           url: "covered_location.png",
-          scaledSize: new google.maps.Size(16, 16)
+          scaledSize: new google.maps.Size(9, 9)
         }
       },
       "4": {
         rasterIcon: {
           url: "covered_location.png",
-          scaledSize: new google.maps.Size(20, 20)
+          scaledSize: new google.maps.Size(12, 12)
         }
       }
     },
@@ -53,31 +51,31 @@ function initMap(): void {
       "0": {
         rasterIcon: {
           url: "uncovered_location.png",
-          scaledSize: new google.maps.Size(5, 5)
+          scaledSize: new google.maps.Size(2, 2)
         }
       },
       "1": {
         rasterIcon: {
           url: "uncovered_location.png",
-          scaledSize: new google.maps.Size(8, 8)
+          scaledSize: new google.maps.Size(3, 3)
         }
       },
       "2": {
         rasterIcon: {
           url: "uncovered_location.png",
-          scaledSize: new google.maps.Size(12, 12)
+          scaledSize: new google.maps.Size(6, 6)
         }
       },
       "3": {
         rasterIcon: {
           url: "uncovered_location.png",
-          scaledSize: new google.maps.Size(16, 16)
+          scaledSize: new google.maps.Size(9, 9)
         }
       },
       "4": {
         rasterIcon: {
           url: "uncovered_location.png",
-          scaledSize: new google.maps.Size(20, 20)
+          scaledSize: new google.maps.Size(12, 12)
         }
       }
     }
@@ -88,73 +86,182 @@ function initMap(): void {
   }
 
   function getRandomInt(min, max) {
-    return Math.ceil(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    return Math.round(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   }
 
-  var clusteredCoveredLocations = [];
-  var clusteredUncoveredLocations = [];
+  const locationsNumberParameter = new URLSearchParams(
+    window.location.search
+  ).get("number_of_locations");
 
-  for (let i = 0; i < 10000; i++) {
-    var latitude = getRandom(33, 47);
-    var longitude = getRandom(-68, -126);
-    var covered = Math.random() < 0.5;
-    var size = getRandomInt(0, 4);
-    var data =
+  const locationsNumber = parseInt(
+    locationsNumberParameter == null ? "600" : locationsNumberParameter,
+    10
+  );
+
+  const organizationId = new URLSearchParams(window.location.search).get(
+    "organization_id"
+  );
+
+  function drawPieSlice(settings) {
+    let d = "";
+
+    const firstCircumferenceX =
+      settings.centreX + settings.radius * Math.sin(settings.startAngleRadians);
+    const firstCircumferenceY =
+      settings.centreY - settings.radius * Math.cos(settings.startAngleRadians);
+    const secondCircumferenceX =
+      settings.centreX +
+      settings.radius *
+        Math.sin(settings.startAngleRadians + settings.sweepAngleRadians);
+    const secondCircumferenceY =
+      settings.centreY -
+      settings.radius *
+        Math.cos(settings.startAngleRadians + settings.sweepAngleRadians);
+
+    // move to centre
+    d += "M" + settings.centreX + "," + settings.centreY + " ";
+    // line to first edge
+    d += "L" + firstCircumferenceX + "," + firstCircumferenceY + " ";
+    // arc
+    // Radius X, Radius Y, X Axis Rotation, Large Arc Flag, Sweep Flag, End X, End Y
+    d += `A ${settings.radius},${settings.radius} 0 ${
+      parseFloat(settings.sweepAngleRadians) >= Math.PI ? 1 : 0
+    },1 ${secondCircumferenceX},${secondCircumferenceY} `;
+    // close path
+    d += "Z";
+
+    return d;
+  }
+
+  var locationMarkers: google.maps.Marker[] = [];
+  var locationClusterMarkers: google.maps.Marker[] = [];
+
+  class StateData {
+    public locationCount: number;
+    constructor(public latitude: number, public longitude: number) {
+      this.locationCount = 0;
+    }
+  }
+
+  const states: Record<string, StateData> = {
+    AL: new StateData(32.834722, -86.633333),
+    AK: new StateData(64.731667, -152.47),
+    AZ: new StateData(34.566667, -111.856111),
+    AR: new StateData(34.742222, -92.285833),
+    CA: new StateData(37.166111, -119.449444),
+    CO: new StateData(39, -105.545167),
+    CT: new StateData(41.81, -72.73),
+    DE: new StateData(38.98, -75.511667),
+    DC: new StateData(38.904167, -77.016111),
+    FL: new StateData(28.681389, -82.46),
+    GA: new StateData(32.662111, -83.438306),
+    HI: new StateData(20.86774, -156.61706),
+    ID: new StateData(44.269722, -114.734444),
+    IL: new StateData(39.739306, -89.503639),
+    IN: new StateData(39.766028, -86.441278),
+    IA: new StateData(42.037778, -93.466389),
+    KS: new StateData(38.466667, -98.766667),
+    KY: new StateData(37.62, -84.87),
+    LA: new StateData(31, -92),
+    ME: new StateData(45.253333, -69.233333),
+    MD: new StateData(39, -77),
+    MA: new StateData(42.377117, -71.925258),
+    MI: new StateData(44.34, -85.58),
+    MN: new StateData(46.49, -94.07),
+    MO: new StateData(38.22, -92.43),
+    MS: new StateData(32.75, -89.52),
+    MT: new StateData(47.064444, -109.666944),
+    NE: new StateData(41.525, -99.861667),
+    NV: new StateData(39.95, -117.03),
+    NH: new StateData(43.52, -71.42),
+    NJ: new StateData(40.07, -74.558333),
+    NM: new StateData(34.438056, -106.112222),
+    NY: new StateData(42.91, -75.67),
+    NC: new StateData(35.7, -79.26),
+    ND: new StateData(47.58, -100.34),
+    OH: new StateData(40.304444, -82.695556),
+    OK: new StateData(35.48, -97.53),
+    OR: new StateData(44.13, -120.36),
+    PA: new StateData(40.91, -77.82),
+    RI: new StateData(41.671667, -71.576667),
+    SC: new StateData(33.836944, -80.925),
+    SD: new StateData(44.39, -99.99),
+    TN: new StateData(35.84, -86.42),
+    TX: new StateData(31.391533, -99.17065),
+    UT: new StateData(39.308056, -111.638889),
+    VT: new StateData(44.27, -72.62),
+    VA: new StateData(37.57, -78.53),
+    WA: new StateData(47.88, -120.64),
+    WV: new StateData(38.7, -80.73),
+    WI: new StateData(44.437778, -90.130186),
+    WY: new StateData(43, -107.545167)
+  };
+
+  const stateKeys = Object.keys(states);
+
+  for (let i = 0; i < locationsNumber; i++) {
+    const state = stateKeys[getRandomInt(0, stateKeys.length - 1)];
+
+    if (states[state].locationCount === undefined) {
+      states[state].locationCount = 1;
+    } else {
+      states[state].locationCount += 1;
+    }
+    let latitude = states[state].latitude + getRandom(-1, 1);
+    let longitude = states[state].longitude + getRandom(-1, 1);
+    let covered = Math.random() < 0.5;
+    let size = getRandomInt(0, 3);
+    let data =
       "R12: $ <b>xxxxx</b><br/>Covered<br/>Street<br/>City, state<br/>View Location";
 
     const marker = new google.maps.Marker({
       position: new google.maps.LatLng(latitude, longitude),
       icon: icons[covered.toString()][size].rasterIcon,
-      map: map
+      map: map,
+      visible: false,
+      optimized: true,
+      state: state
     });
-    var infowindow = new google.maps.InfoWindow({
+    locationMarkers.push(marker);
+    const infowindow = new google.maps.InfoWindow({
       content: data
     });
     google.maps.event.addListener(marker, "click", function () {
       infowindow.open(map, marker);
     });
-    if (size < 3) {
-      if (covered) {
-        clusteredCoveredLocations.push(marker);
-      } else {
-        clusteredUncoveredLocations.push(marker);
-      }
-    }
   }
-
-  const uncoveredClusterRenderer = {
-    render: ({ count, position }) =>
-      new google.maps.Marker({
-        label: { text: String(count), color: "white", fontSize: "10px" },
-        position,
-        // adjust zIndex to be above other markers
-        zIndex: Number(google.maps.Marker.MAX_ZINDEX) + count,
-        icon: "uncovered_cluster.png"
-      })
-  };
-
-  new MarkerClusterer({
-    markers: clusteredCoveredLocations,
-    map,
-    renderer: uncoveredClusterRenderer
-  });
-
-  const coveredClusterRenderer = {
-    render: ({ count, position }) =>
-      new google.maps.Marker({
-        label: { text: String(count), color: "white", fontSize: "10px" },
-        position,
-        // adjust zIndex to be above other markers
-        zIndex: Number(google.maps.Marker.MAX_ZINDEX) + count,
-        icon: "covered_cluster.png"
-      })
-  };
-
-  new MarkerClusterer({
-    markers: clusteredUncoveredLocations,
-    map,
-    renderer: coveredClusterRenderer
-  });
+  if (locationsNumber >= 10000) {
+    for (const state in states) {
+      const markerPosition = new google.maps.LatLng(
+        states[state].latitude,
+        states[state].longitude
+      );
+      var marker = new google.maps.Marker({
+        position: markerPosition,
+        icon: {
+          anchor: new google.maps.Point(33, 33),
+          url: "covered_cluster.png"
+        },
+        label: states[state].locationCount.toString(),
+        map: map,
+        // optimized: true
+        state: state
+      });
+      locationClusterMarkers.push(marker);
+      google.maps.event.addListener(marker, "click", function () {
+        map.setCenter(markerPosition);
+        map.setZoom(8);
+        locationClusterMarkers.forEach((marker) => marker.setVisible(false));
+        locationMarkers.forEach((marker) => {
+          if (marker.state === state) {
+            marker.setVisible(true);
+          }
+        });
+      });
+    }
+  } else {
+    locationMarkers.forEach((marker) => marker.setVisible(true));
+  }
 
   const legend = document.getElementById("legend") as HTMLElement;
 
@@ -245,6 +352,12 @@ function initMap(): void {
         stylers: [{ visibility: "off" }]
       }
     ]
+  });
+  map.addListener("zoom_changed", () => {
+    if (map.getZoom() < 7) {
+      locationMarkers.forEach((marker) => marker.setVisible(false));
+      locationClusterMarkers.forEach((marker) => marker.setVisible(true));
+    }
   });
 }
 
